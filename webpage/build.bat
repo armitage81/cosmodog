f194d907c65b2625fe42baac1f9ac9d00c0941a4
armitage81 <contact@anton-afanasjew.de>
2017-09-29 13:49:59 +0200

    Fixed problems with the webpage

--- webpage/build.bat
+++ webpage/build.bat
@@ -1,2 +1,3 @@
 java -Dlog4j.configuration=file:templateengine/log4j.properties -jar templateengine/TemplateEngine.jar "html\templates\template.txt" "html\variables\variables.txt" "html\bigVariables" "html\output"
-xcopy html\pics html\output\pics /e /i /h 
+xcopy html\pics html\output\pics /e /i /h
+copy html\index.html html\output\index.html
