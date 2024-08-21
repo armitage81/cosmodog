65f9fe44b03fa9a701fd5549107ebf91ebf8ccb7
Afanasjew <anton.afanasjew@siemens.com>
2024-08-21 23:13:21 +0200

    Commented some code to remember its functionality.

--- src/antonafanasjew/cosmodog/actions/cutscenes/PauseDecoratorAction.java
+++ src/antonafanasjew/cosmodog/actions/cutscenes/PauseDecoratorAction.java
@@ -9,36 +9,89 @@ import antonafanasjew.cosmodog.actions.AsyncActionType;
 import antonafanasjew.cosmodog.actions.VariableLengthAsyncAction;
 import antonafanasjew.cosmodog.actions.wait.WaitAction;
 
+import java.io.Serial;
+
+/**
+ * Represents a pause decorator action that wraps an underlying action with an optional pause before it and an optional
+ * pause after it. That could be used, for instance, to create a dramatic delay before an important action.
+ * <p>
+ * This is a variable length action. It uses its local action registry to register the underlying action and the pauses
+ * as phases. The action is considered finished when all phases have been executed.
+ */
 public class PauseDecoratorAction  extends VariableLengthAsyncAction {
 
+	@Serial
 	private static final long serialVersionUID = 5171259029091809078L;
 
-	private int durationBefore;
-	private int durationAfter;
-	private AsyncAction underlyingAsyncAction;
-	
-	private ActionRegistry actionPhaseRegistry = new ActionRegistry();
-	
+	/**
+	 * The duration of the pause before the underlying action is executed.
+	 */
+	private final int durationBefore;
+
+	/**
+	 * The duration of the pause after the underlying action is executed.
+	 */
+	private final int durationAfter;
+
+	/**
+	 * The underlying action that is executed in between the pauses.
+	 */
+	private final AsyncAction underlyingAsyncAction;
+
+	/**
+	 * The local action registry that is used to register the underlying action and the pauses as phases.
+	 */
+	private final ActionRegistry actionPhaseRegistry = new ActionRegistry();
+
+	/**
+	 * Creates a new pause decorator action with the specified durations and the underlying action.
+	 *
+	 * @param durationBefore the duration of the pause before the underlying action is executed (in milliseconds).
+	 * @param durationAfter  the duration of the pause after the underlying action is executed (in milliseconds).
+	 * @param underlyingAsyncAction the underlying action that is executed in between the pauses.
+	 */
 	public PauseDecoratorAction(int durationBefore, int durationAfter, AsyncAction underlyingAsyncAction) {
 		this.durationBefore = durationBefore;
 		this.durationAfter = durationAfter;
 		this.underlyingAsyncAction = underlyingAsyncAction;
 	}
-	
+
+	/**
+	 * Initializes the execution of the action.
+	 * Registers the underlying action and the pauses as phases in the local action registry so they can
+	 * be executed in order.
+	 */
 	@Override
 	public void onTrigger() {
-		
 		actionPhaseRegistry.registerAction(AsyncActionType.CUTSCENE, new WaitAction(durationBefore));
 		actionPhaseRegistry.registerAction(AsyncActionType.CUTSCENE, underlyingAsyncAction);
 		actionPhaseRegistry.registerAction(AsyncActionType.CUTSCENE, new WaitAction(durationAfter));
-		
 	}
-	
+
+	/**
+	 * Updates the action based on the passed time.
+	 * <p>
+	 * In this case, the update is simply delegated to the phase registry of this action.
+	 * This way, the phases of this action are updated in the correct order: First, the first pause is being executed.
+	 * Then, the underlying action is executed. Finally, the second pause is being executed.
+	 *
+	 * @param before Time offset of the last update as compared to the start of the action.
+	 * @param after Time offset of the current update. after - before = time passed since the last update.
+	 * @param gc GameContainer instance forwarded by the game state's update method.
+	 * @param sbg StateBasedGame instance forwarded by the game state's update method.
+	 */
 	@Override
 	public void onUpdate(int before, int after, GameContainer gc, StateBasedGame sbg) {
 		actionPhaseRegistry.update(after - before, gc, sbg);
 	}
-	
+
+	/**
+	 * States whether the action is finished.
+	 * <p>
+	 * This is the case when the last phase of the action has been unregistered and the phase registry is empty.
+	 *
+	 * @return true if the action has finished, false otherwise.
+	 */
 	@Override
 	public boolean hasFinished() {
 		return !actionPhaseRegistry.isActionRegistered(AsyncActionType.CUTSCENE);
--- src/antonafanasjew/cosmodog/actions/cutscenes/WormAttackAction.java
+++ src/antonafanasjew/cosmodog/actions/cutscenes/WormAttackAction.java
@@ -8,16 +8,40 @@ import antonafanasjew.cosmodog.SoundResources;
 import antonafanasjew.cosmodog.actions.FixedLengthAsyncAction;
 import antonafanasjew.cosmodog.util.ApplicationContextUtils;
 
+import java.io.Serial;
+
 /**
- * Represents the game action of the attacking worm animation. 
- *
+ * Represents the game action of the attacking worm animation.
+ * A worm attack happens if the player walks on snow in a specific area for too long. It immediately kills the player.
+ * <p>
+ * This is a fixed length action. It is considered finished when the duration has passed.
+ * <p>
+ * The action models two effects: The earthquake and the worm attack. The latter is represented by the worm attack transition.
+ * This transition mainly defines the height of the worm for every frame, meaning how much of the worm is visible above the snow
+ * when it lurches up.
  */
 public class WormAttackAction extends FixedLengthAsyncAction {
 
+
+	@Serial
 	private static final long serialVersionUID = 8882906074849186691L;
 
+	/**
+	 * Indicates whether the earthquake sound has been triggered (before worm appears).
+	 * This is to avoid multiple sound plays during the same action.
+	 */
 	private boolean triggeredEarthquake = false;
+
+	/**
+	 * Indicates whether the worm sound has been triggered (growling).
+	 * This is to avoid multiple sound plays during the same action.
+	 */
 	private boolean triggeredWorm = false;
+
+	/**
+	 * Indicates whether the second earthquake sound has been triggered (after worm disappears).
+	 * This is to avoid multiple sound plays during the same action.
+	 */
 	private boolean triggeredExitEarthquake = false;
 	
 	/**
@@ -29,7 +53,7 @@ public class WormAttackAction extends FixedLengthAsyncAction {
 	public static class WormAttackTransition {
 		
 		/**
-		 * Percentage of the action completion.
+		 * Percentage of the action completion. It is a value between 0.0 and 1.0.
 		 */
 		public float percentage = 0.0f;
 		
@@ -37,42 +61,48 @@ public class WormAttackAction extends FixedLengthAsyncAction {
 		 * Indicates the current height of the worm.
 		 * This value can be used to render the worm animation
 		 * at the proper place.
+		 * <p>
+		 * The function time -> height is implemented to make the lurching attack upwards plausible.
+		 * It follows the pattern:
+		 * - First, there is no worm for a while.
+		 * - 
 		 */
 		public float wormHeightPercentage() {
 			
 			if (percentage < 0.2) {
-				float retVal = 0;
-				return retVal;
+                return (float) 0;
 			}
 			
 			if (percentage < 0.3) {
-				float retVal = (percentage - 0.2f) / (0.1f) * 0.8f;
-				return retVal;
+                return (percentage - 0.2f) / (0.1f) * 0.8f;
 			}
 			
 			if (percentage < 0.5) {
-				float retVal = 0.8f + (percentage - 0.3f) / (0.2f) * 0.2f;
-				return retVal;
+                return 0.8f + (percentage - 0.3f) / (0.2f) * 0.2f;
 			}
 			
 			if (percentage < 0.6) {
-				float retVal = 1.0f;
-				return retVal;
+                return 1.0f;
 			}
 			
 			if (percentage < 1.0) {
-				float retVal = 1.0f - (percentage - 0.6f) / (0.4f) * 1.0f;
-				return retVal;
+                return 1.0f - (percentage - 0.6f) / (0.4f);
 			}
-			
-			float retVal = 0.0f;
-			return retVal;
+
+            return 0.0f;
 		}
 		
 	}
 
 	private WormAttackTransition transition;
-	
+
+	/**
+	 * Returns the underlying transition.
+	 */
+	public WormAttackTransition getTransition() {
+		return transition;
+	}
+
 	/**
 	 * Initializes the worm attack action with the given duration.
 	 */
@@ -116,13 +146,6 @@ public class WormAttackAction extends FixedLengthAsyncAction {
 	public void onEnd() {
 		transition = null;
 		ApplicationContextUtils.getPlayer().setLife(0);
-	}
-
-	/**
-	 * Returns the underlying transition.
-	 */
-	public WormAttackTransition getTransition() {
-		return transition;
 	}
 
 }
