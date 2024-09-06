58e9defbcecafe7388077cad05fb5355b2dcad08
Armitage81 <contact@anton-afanasjew.de>
2024-09-06 22:32:37 +0200

    #48 - Wrong collision in a lake.

---
+++ src/integration_test/TestIssue48WrongWaterCollision.java
@@ -0,0 +1,37 @@
+package integration_test;
+
+import antonafanasjew.cosmodog.globals.CosmodogModelHolder;
+import antonafanasjew.cosmodog.model.actors.Player;
+import antonafanasjew.cosmodog.model.actors.Vehicle;
+import antonafanasjew.cosmodog.model.inventory.BoatInventoryItem;
+import antonafanasjew.cosmodog.model.inventory.InventoryItemType;
+import antonafanasjew.cosmodog.model.inventory.VehicleInventoryItem;
+import antonafanasjew.cosmodog.player.AbstractPlayerBuilder;
+import antonafanasjew.cosmodog.player.PlayerBuilder;
+import org.newdawn.slick.SlickException;
+
+public class TestIssue48WrongWaterCollision {
+
+	public static void main(String[] args) throws SlickException {
+		
+		PlayerBuilder playerBuilder = new AbstractPlayerBuilder() {
+			
+			@Override
+			protected void updatePlayer(Player player) {
+				player.setPositionX(38);
+				player.setPositionY(269);
+				player.setMaxLife(100);
+				player.setLife(100);
+				VehicleInventoryItem vii = new VehicleInventoryItem(new Vehicle());
+				player.getInventory().put(InventoryItemType.VEHICLE, vii);
+				player.getInventory().put(InventoryItemType.BOAT, new BoatInventoryItem());
+				
+			}
+		};
+
+		CosmodogModelHolder.replacePlayerBuilder(playerBuilder);
+
+		TestStarter.main(args);
+	}
+	
+}
