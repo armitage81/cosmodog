1222c4f6176d3dffd51020fd939b69adde65ad9b
Armitage81 <contact@anton-afanasjew.de>
2024-09-13 22:43:32 +0200

    #68 - Added a teleport from Sokoban riddle chain back to the worm area.

---
+++ src/integration_test/TestTeleportSokobanEndToWormArea.java
@@ -0,0 +1,35 @@
+package integration_test;
+
+import antonafanasjew.cosmodog.globals.CosmodogModelHolder;
+import antonafanasjew.cosmodog.model.actors.Player;
+import antonafanasjew.cosmodog.model.inventory.DebuggerInventoryItem;
+import antonafanasjew.cosmodog.model.inventory.InventoryItemType;
+import antonafanasjew.cosmodog.model.inventory.JacketInventoryItem;
+import antonafanasjew.cosmodog.model.inventory.SkiInventoryItem;
+import antonafanasjew.cosmodog.player.AbstractPlayerBuilder;
+import antonafanasjew.cosmodog.player.PlayerBuilder;
+import org.newdawn.slick.SlickException;
+
+public class TestTeleportSokobanEndToWormArea {
+
+	public static void main(String[] args) throws SlickException {
+		
+		PlayerBuilder playerBuilder = new AbstractPlayerBuilder() {
+			
+			@Override
+			protected void updatePlayer(Player player) {
+				player.setPositionX(335);
+				player.setPositionY(303);
+				player.setLife(1);
+				player.getInventory().put(InventoryItemType.SKI, new SkiInventoryItem());
+				player.getInventory().put(InventoryItemType.JACKET, new JacketInventoryItem());
+				player.getInventory().put(InventoryItemType.DEBUGGER, new DebuggerInventoryItem("111/355;111/357;111/349"));
+			}
+		};
+
+		CosmodogModelHolder.replacePlayerBuilder(playerBuilder);
+
+		TestStarter.main(args);
+	}
+	
+}
