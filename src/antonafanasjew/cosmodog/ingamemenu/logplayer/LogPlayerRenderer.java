99ef4d098c97b0ece02c026b513732409bd7fca4
Afanasjew <anton.afanasjew@siemens.com>
2024-09-18 14:58:21 +0200

    Commented the log player renderer

--- src/antonafanasjew/cosmodog/ingamemenu/logplayer/LogPlayerRenderer.java
+++ src/antonafanasjew/cosmodog/ingamemenu/logplayer/LogPlayerRenderer.java
@@ -24,6 +24,17 @@ import antonafanasjew.cosmodog.util.ApplicationContextUtils;
 import antonafanasjew.cosmodog.util.ImageUtils;
 import antonafanasjew.cosmodog.util.TextBookRendererUtils;
 
+/**
+ * Represents the renderer that renders the log menu, including all logs (found and not found) and the
+ * contents of the selected log.
+ * <p>
+ * What to do if a new log is added to the game:
+ * <p>
+ * It is important to check that the log is not exceeding the maximal number of logs in a series
+ * and also that the series (if new) is not exceeding the maximal number of series.
+ * Also, if a new series is added, check that the indexes of labels and other series are updated.
+ * If a new category of series is added, then check that it is added everywhere like other categories are.
+ */
 public class LogPlayerRenderer implements Renderer {
 
 	/**
@@ -35,7 +46,7 @@ public class LogPlayerRenderer implements Renderer {
 	 * There can be maximally that many log series.
 	 */
 	private static final int MAX_NUMBER_OF_SERIES = 20;
-	private static final String[] LOG_CATEGORY_LABELS = new String[] {"Tutorials", "Story", "Logs", "Other"};
+	private static final int LOG_CATEGORY_LABELS_SIZE = 4;
 	private static final int ROWS_FOR_LABEL = 2;
 
 
@@ -65,10 +76,10 @@ public class LogPlayerRenderer implements Renderer {
 		LogPlayerInputState logPlayerInputState = (LogPlayerInputState)renderingParameter;
 
 		//Currently selected series of logs (row).
-		int seriesNumber = logPlayerInputState.getSeriesNumber();
+		int selectedSeriesNumber = logPlayerInputState.getSeriesNumber();
 
 		//Currently selected log in a series (column).
-		int logNumber = logPlayerInputState.getLogNumber();
+		int selectedLogNumber = logPlayerInputState.getLogNumber();
 
 		//Draws the bluish frame around the log overview. It contains two sections: the log overview and the log content.
 		ImageUtils.renderImage(gameContainer, graphics, "ui.ingame.ingamelogs", inGameMenuContentDrawingContext);
@@ -76,7 +87,7 @@ public class LogPlayerRenderer implements Renderer {
 		//The log overview contains blocks for each log series, but also labels for log categories ("Hints", "Story", "Logs", "Other").
 		//A label can use a block of 2 or more rows.
 		//Example: There are 8 series, 4 categories, and 2 rows per label. This means that there are 16 rows in total.
-		int numberOfRows = MAX_NUMBER_OF_SERIES + LOG_CATEGORY_LABELS.length * ROWS_FOR_LABEL;
+		int numberOfRows = MAX_NUMBER_OF_SERIES + LOG_CATEGORY_LABELS_SIZE * ROWS_FOR_LABEL;
 
 		//The number of log series, regardless of categories.
 		int noOfSeries = gameLogs.getSeriesNames().size();
@@ -99,50 +110,69 @@ public class LogPlayerRenderer implements Renderer {
 		TextBookRendererUtils.renderLeftAlignedLabel(gameContainer, graphics, story);
 		TextBookRendererUtils.renderLeftAlignedLabel(gameContainer, graphics, logs);
 		TextBookRendererUtils.renderLeftAlignedLabel(gameContainer, graphics, other);
-		
+
+		//We iterate through maximal (that is potential) number of series while there could be less of them in reality.
 		for (short i = 0; i < MAX_NUMBER_OF_SERIES; i++) {
-			
+
+			//If there are less series than the maximal number, we skip the rest of the loop.
 			if (i >= noOfSeries) {
 				continue;
 			}
-			
+
+			//Series 0 is not drawn at row 0, because there is a category label taking some rows first.
+			//Series 5 could have multiple other labels above it.
+			//That's why we have to calculate the actual row number for each series.
 			int rowNumber = entryPositionsByIndexes.apply(i);
-			
+
+			//numberOfRows includes rows for all labels and all series.
+			//Each series will be drawn in the row rowNumber.
 			DrawingContext oneSeriesDc = new TileDrawingContext(logOverviewDrawingContext, 1, numberOfRows, 0, rowNumber);
+
+			//Each series of logs can maximally contain MAX_NUMBER_OF_LOGS_IN_SERIES logs.
+			//Most of them will contain less logs, though.
 			for (short j = 0; j < MAX_NUMBER_OF_LOGS_IN_SERIES; j++) {
 				
 				String seriesName = gameLogs.getSeriesNames().get(i);
 				List<GameLog> gameLogsForSeries = gameLogs.getGameLogsForSeries(seriesName);
-				
+
+				//If the series does not contain as many logs as MAX_NUMBER_OF_LOGS_IN_SERIES, we skip the rest of the loop.
 				if (j >= gameLogsForSeries.size()) {
 					continue;
 				}
-				
+
+				//Each log in the series will be drawn in its own context as a horizontal slice of the row.
 				DrawingContext oneLogDc = new TileDrawingContext(oneSeriesDc, MAX_NUMBER_OF_LOGS_IN_SERIES, 1, j, 0);
 
 				GameLog gameLog = gameLogsForSeries.get(j);
 
+				//This flag defines whether a log has been found by the player, or not.
+				//There are two types of logs: specific logs and series logs. Specific logs can be found independently
+				//while series logs are found in an order regardless of the collectible on the map
 				boolean found;
 				if (GameLogs.SPECIFIC_LOGS_SERIES.contains(seriesName)) {
 					found = logPlayer.allFoundSpecificLogs(seriesName).contains(j);
 				} else {
 					found = logPlayer.noOfFoundLogsForSeries(seriesName) > j;
 				}
-				
-				if (i == seriesNumber && j == logNumber) {
-					
+
+				//The selected log will be emphasized by a kind of cursor.
+				if (i == selectedSeriesNumber && j == selectedLogNumber) {
+
+					//Selected log is drawn with a red frame whether it is found or not.
 					graphics.setColor(Color.red);
 					graphics.drawRect(oneLogDc.x(), oneLogDc.y(), oneLogDc.w(), oneLogDc.h());
-					
-					
+
+					//Selected and found log will be presented on the right half of the menu screen.
 					if (found) {
-						
+
+						//First, log header is printed. If the log has more than one page, the page number is added.
 						String logHeader = gameLog.getHeader();
-						
 						if (logPlayerInputState.getCurrentLogBook().size() > 1) {
 							logHeader = logHeader + " (Page " + (logPlayerInputState.getCurrentLogBook().getCurrentPage() + 1) + "/" + logPlayerInputState.getCurrentLogBook().size() + ")";
 						}
-						
+
+						//The log frame has three elements: header, content, and a controls hint.
+						//The elements are arranged vertically while the content is in the middle and takes the most place.
 						String logHint = "Press [ENTER] to turn page.";
 						
 						DrawingContext titleDrawingContext = DrawingContextProviderHolder.get().getDrawingContextProvider().logPlayerTitleDrawingContext();
@@ -153,7 +183,7 @@ public class LogPlayerRenderer implements Renderer {
 						Book header = TextPageConstraints.fromDc(titleDrawingContext).textToBook(logHeader, subHeaderFontType);
 						TextBookRendererUtils.renderDynamicTextPage(gameContainer, graphics, header);
 						
-						
+						//The controls hint is only shown if the log has more than one page.
 						if (logPlayerInputState.getCurrentLogBook().size() > 1) {
 							Book hint = TextPageConstraints.fromDc(controlsHintDrawingContext).textToBook(logHint, fontTypeLogControlsHint);
 							TextBookRendererUtils.renderDynamicTextPage(gameContainer, graphics, hint);
@@ -162,9 +192,9 @@ public class LogPlayerRenderer implements Renderer {
 						Book content = logPlayerInputState.getCurrentLogBook();
 						TextBookRendererUtils.renderDynamicTextPage(gameContainer, graphics, content);
 					}
-					
 				}
-				
+
+				//If the log is not selected it is just drawn green or gray depending on whether it has been found or not.
 				oneLogDc = new CenteredDrawingContext(oneLogDc, 5);
 				graphics.setColor(found ? Color.green : Color.gray);
 				graphics.fillRect(oneLogDc.x(), oneLogDc.y(), oneLogDc.w(), oneLogDc.h());
