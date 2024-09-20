82c3447959036e24e3d11ba009016ac9b4daf1bc
Armitage81 <contact@anton-afanasjew.de>
2024-09-20 13:20:33 +0200

    Tested all small fir tips

--- src/integration_test/TestDifficultSokobanRiddleHelp.java
+++ src/integration_test/TestDifficultSokobanRiddleHelp.java
@@ -18,7 +18,6 @@ public class TestDifficultSokobanRiddleHelp {
 				player.setPositionY(145);
 				player.setMaxLife(100);
 				player.setLife(100);
-				player.getLogPlayer().addSpecificLog("unsorted", (short)0);
 				for (int i = 0; i < 2999; i++) {
 					player.getGameProgress().addInfobit();
 				}
