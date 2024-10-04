134d2422d979fd03382b2dcbef6e3ded7f6a4276
Armitage81 <contact@anton-afanasjew.de>
2024-10-04 21:20:10 +0200

    #92 - Added a fast exit after getting the Geiger Counter in the parking lot.

---
+++ src/integration_test/TestFastExitAfterGettingGeigerCounter.java
@@ -0,0 +1,49 @@
+package integration_test;
+
+import antonafanasjew.cosmodog.globals.CosmodogModelHolder;
+import antonafanasjew.cosmodog.model.actors.Player;
+import antonafanasjew.cosmodog.model.actors.Vehicle;
+import antonafanasjew.cosmodog.model.inventory.DebuggerInventoryItem;
+import antonafanasjew.cosmodog.model.inventory.GeigerZaehlerInventoryItem;
+import antonafanasjew.cosmodog.model.inventory.InventoryItemType;
+import antonafanasjew.cosmodog.model.inventory.VehicleInventoryItem;
+import antonafanasjew.cosmodog.player.AbstractPlayerBuilder;
+import antonafanasjew.cosmodog.player.PlayerBuilder;
+import org.newdawn.slick.SlickException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestFastExitAfterGettingGeigerCounter {
+
+	public static void main(String[] args) throws SlickException {
+		
+		PlayerBuilder playerBuilder = new AbstractPlayerBuilder() {
+			
+			@Override
+			protected void updatePlayer(Player player) {
+				player.setPositionX(39);
+				player.setPositionY(212);
+				player.setMaxLife(100);
+				player.setLife(100);
+
+				player.getInventory().put(InventoryItemType.GEIGERZAEHLER, new GeigerZaehlerInventoryItem());
+
+				List<String> debuggerPositions = new ArrayList<>();
+
+				debuggerPositions.add("41/222");
+				debuggerPositions.add("40/227");
+				debuggerPositions.add("39/212");
+
+
+
+				player.getInventory().put(InventoryItemType.DEBUGGER, new DebuggerInventoryItem(String.join(";", debuggerPositions)));
+			}
+		};
+
+		CosmodogModelHolder.replacePlayerBuilder(playerBuilder);
+
+		TestStarter.main(args);
+	}
+	
+}
