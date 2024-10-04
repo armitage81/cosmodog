e4343f9e0654dec69ba64e55b5693b7a2340f3fa
Armitage81 <contact@anton-afanasjew.de>
2024-10-04 12:10:14 +0200

    #91 - Fixed Geiger Counter description.

--- src/antonafanasjew/cosmodog/listener/movement/pieceinteraction/GeigerZaehlerInteraction.java
+++ src/antonafanasjew/cosmodog/listener/movement/pieceinteraction/GeigerZaehlerInteraction.java
@@ -33,7 +33,7 @@ public class GeigerZaehlerInteraction extends ToolInteraction {
 		
 		GeigerZaehlerInventoryItem geigerCounter = (GeigerZaehlerInventoryItem)player.getInventory().get(InventoryItemType.GEIGERZAEHLER);
 		if (geigerCounter == null) {
-			return "You found a Geiger counter. It indicates radiation. The 'RAD' value shows the number of contaminated adjacent tiles including your own position.";
+			return "You found a Geiger counter. It indicates radiation. The 'RAD' value shows the contaminated adjacent tiles including your own position.";
 		} else {
 			return "You found the radiation suit. It will protect you from radioactive rays.";
 		}
