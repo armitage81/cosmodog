da0bd237a7cfa4eb3382e9a1992d1c5f2906fb0f
Armitage81 <contact@anton-afanasjew.de>
2024-10-04 21:34:10 +0200

    #99 - Fast exit after a long march through radioactive field.

---
+++ src/integration_test/TestFastExitAfterLongMarchInRadioactiveLabyrinth.java
@@ -0,0 +1,47 @@
+package integration_test;
+
+import antonafanasjew.cosmodog.globals.CosmodogModelHolder;
+import antonafanasjew.cosmodog.model.actors.Player;
+import antonafanasjew.cosmodog.model.inventory.DebuggerInventoryItem;
+import antonafanasjew.cosmodog.model.inventory.GeigerZaehlerInventoryItem;
+import antonafanasjew.cosmodog.model.inventory.InventoryItemType;
+import antonafanasjew.cosmodog.player.AbstractPlayerBuilder;
+import antonafanasjew.cosmodog.player.PlayerBuilder;
+import org.newdawn.slick.SlickException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestFastExitAfterLongMarchInRadioactiveLabyrinth {
+
+	public static void main(String[] args) throws SlickException {
+		
+		PlayerBuilder playerBuilder = new AbstractPlayerBuilder() {
+			
+			@Override
+			protected void updatePlayer(Player player) {
+				player.setPositionX(107);
+				player.setPositionY(153);
+				player.setMaxLife(100);
+				player.setLife(100);
+
+				player.getInventory().put(InventoryItemType.GEIGERZAEHLER, new GeigerZaehlerInventoryItem());
+
+				List<String> debuggerPositions = new ArrayList<>();
+
+				debuggerPositions.add("105/157");
+				debuggerPositions.add("103/159");
+				debuggerPositions.add("107/153");
+
+
+
+				player.getInventory().put(InventoryItemType.DEBUGGER, new DebuggerInventoryItem(String.join(";", debuggerPositions)));
+			}
+		};
+
+		CosmodogModelHolder.replacePlayerBuilder(playerBuilder);
+
+		TestStarter.main(args);
+	}
+	
+}
