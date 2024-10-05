e0f251e1a58de196f0213742f8d54bce0010ae95
Anton Afanasjew <contact@anton-afanasjew.de>
2024-10-05 22:46:46 +0200

    #137 - There is a passage now at the north west edge of the alien base.

---
+++ src/integration_test/TestPassageNorthWestOfAlienBase.java
@@ -0,0 +1,42 @@
+package integration_test;
+
+import antonafanasjew.cosmodog.domains.WeaponType;
+import antonafanasjew.cosmodog.globals.CosmodogModelHolder;
+import antonafanasjew.cosmodog.model.actors.Player;
+import antonafanasjew.cosmodog.model.inventory.*;
+import antonafanasjew.cosmodog.model.upgrades.Weapon;
+import antonafanasjew.cosmodog.player.AbstractPlayerBuilder;
+import antonafanasjew.cosmodog.player.PlayerBuilder;
+import org.newdawn.slick.SlickException;
+
+public class TestPassageNorthWestOfAlienBase {
+
+	public static void main(String[] args) throws SlickException {
+		
+		PlayerBuilder playerBuilder = new AbstractPlayerBuilder() {
+			
+			@Override
+			protected void updatePlayer(Player player) {
+				player.setPositionX(195);
+				player.setPositionY(238);
+				player.setMaxLife(100);
+				player.setLife(100);
+				player.getInventory().put(InventoryItemType.AXE, new AxeInventoryItem());
+				player.getInventory().put(InventoryItemType.PICK, new PickInventoryItem());
+				player.getInventory().put(InventoryItemType.SKI, new SkiInventoryItem());
+				player.getInventory().put(InventoryItemType.MACHETE, new MacheteInventoryItem());
+				player.getInventory().put(InventoryItemType.JACKET, new JacketInventoryItem());
+				player.getInventory().put(InventoryItemType.DYNAMITE, new DynamiteInventoryItem());
+
+				player.getArsenal().addWeaponToArsenal(new Weapon(WeaponType.RPG));
+				player.getArsenal().addWeaponToArsenal(new Weapon(WeaponType.SHOTGUN));
+
+			}
+		};
+
+		CosmodogModelHolder.replacePlayerBuilder(playerBuilder);
+
+		TestStarter.main(args);
+	}
+	
+}
