55a6d7906ff7285c4e81d9bf8f36d50a1a720f02
Anton Afanasjew <contact@anton-afanasjew.de>
2024-10-05 22:24:09 +0200

    #123 - Secret 108 appears now when finding the hidden ledge.

---
+++ src/integration_test/TestSecret108.java
@@ -0,0 +1,41 @@
+package integration_test;
+
+import antonafanasjew.cosmodog.domains.WeaponType;
+import antonafanasjew.cosmodog.globals.CosmodogModelHolder;
+import antonafanasjew.cosmodog.model.actors.Player;
+import antonafanasjew.cosmodog.model.inventory.*;
+import antonafanasjew.cosmodog.model.upgrades.Weapon;
+import antonafanasjew.cosmodog.player.AbstractPlayerBuilder;
+import antonafanasjew.cosmodog.player.PlayerBuilder;
+import org.newdawn.slick.SlickException;
+
+public class TestSecret108 {
+
+	public static void main(String[] args) throws SlickException {
+		
+		PlayerBuilder playerBuilder = new AbstractPlayerBuilder() {
+			
+			@Override
+			protected void updatePlayer(Player player) {
+				player.setPositionX(69);
+				player.setPositionY(384);
+				player.setMaxLife(100);
+				player.setLife(100);
+				player.getInventory().put(InventoryItemType.AXE, new AxeInventoryItem());
+				player.getInventory().put(InventoryItemType.PICK, new PickInventoryItem());
+				player.getInventory().put(InventoryItemType.SKI, new SkiInventoryItem());
+				player.getInventory().put(InventoryItemType.JACKET, new JacketInventoryItem());
+
+				player.getArsenal().addWeaponToArsenal(new Weapon(WeaponType.RPG));
+
+
+				player.getInventory().put(InventoryItemType.DEBUGGER, new DebuggerInventoryItem("293/394;282/377"));
+			}
+		};
+
+		CosmodogModelHolder.replacePlayerBuilder(playerBuilder);
+
+		TestStarter.main(args);
+	}
+	
+}
