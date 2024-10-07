9b0ba4e899924f10dab13b81e9afb182c78e2e75
Anton Afanasjew <contact@anton-afanasjew.de>
2024-10-07 22:28:29 +0200

    #109 - Platform vs turrets secret.

---
+++ src/integration_test/TestPlatformVsTurrets.java
@@ -0,0 +1,36 @@
+package integration_test;
+
+import antonafanasjew.cosmodog.globals.CosmodogModelHolder;
+import antonafanasjew.cosmodog.model.actors.Platform;
+import antonafanasjew.cosmodog.model.actors.Player;
+import antonafanasjew.cosmodog.model.actors.Vehicle;
+import antonafanasjew.cosmodog.model.inventory.InventoryItemType;
+import antonafanasjew.cosmodog.model.inventory.PlatformInventoryItem;
+import antonafanasjew.cosmodog.model.inventory.VehicleInventoryItem;
+import antonafanasjew.cosmodog.player.AbstractPlayerBuilder;
+import antonafanasjew.cosmodog.player.PlayerBuilder;
+import org.newdawn.slick.SlickException;
+
+public class TestPlatformVsTurrets {
+
+	public static void main(String[] args) throws SlickException {
+		
+		PlayerBuilder playerBuilder = new AbstractPlayerBuilder() {
+			
+			@Override
+			protected void updatePlayer(Player player) {
+				player.setPositionX(94);
+				player.setPositionY(184);
+				player.setMaxLife(100);
+				player.setLife(100);
+				PlatformInventoryItem platform = new PlatformInventoryItem(new Platform());
+				player.getInventory().put(InventoryItemType.PLATFORM, platform);
+			}
+		};
+
+		CosmodogModelHolder.replacePlayerBuilder(playerBuilder);
+
+		TestStarter.main(args);
+	}
+	
+}
