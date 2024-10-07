98ca31a0337df231c8798c0f1813c4206f0574b8
Armitage81 <contact@anton-afanasjew.de>
2024-10-07 15:46:36 +0200

    #104 - Changed carpet color in the snow base to have better contrast.

---
+++ src/integration_test/TestRedCarpetContrast.java
@@ -0,0 +1,40 @@
+package integration_test;
+
+import antonafanasjew.cosmodog.globals.CosmodogModelHolder;
+import antonafanasjew.cosmodog.model.actors.Player;
+import antonafanasjew.cosmodog.model.inventory.DebuggerInventoryItem;
+import antonafanasjew.cosmodog.model.inventory.InventoryItemType;
+import antonafanasjew.cosmodog.model.inventory.SkiInventoryItem;
+import antonafanasjew.cosmodog.player.AbstractPlayerBuilder;
+import antonafanasjew.cosmodog.player.PlayerBuilder;
+import org.newdawn.slick.SlickException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestRedCarpetContrast {
+
+	public static void main(String[] args) throws SlickException {
+		
+		PlayerBuilder playerBuilder = new AbstractPlayerBuilder() {
+			
+			@Override
+			protected void updatePlayer(Player player) {
+				player.setPositionX(255);
+				player.setPositionY(359);
+				player.setMaxLife(100);
+				player.setLife(100);
+				player.getInventory().put(InventoryItemType.SKI, new SkiInventoryItem());
+
+				List<String> debuggerPositions = new ArrayList<>();
+				debuggerPositions.add("255/359");
+				player.getInventory().put(InventoryItemType.DEBUGGER, new DebuggerInventoryItem(String.join(";", debuggerPositions)));
+			}
+		};
+
+		CosmodogModelHolder.replacePlayerBuilder(playerBuilder);
+
+		TestStarter.main(args);
+	}
+	
+}
