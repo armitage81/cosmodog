44c627aed5237e52473dc4ee9f5b73ddfe103bc9
Armitage81 <contact@anton-afanasjew.de>
2024-10-08 10:22:56 +0200

    #106 - Added snow drifts under rocks.

---
+++ src/integration_test/TestSnowDriftsOnRocks.java
@@ -0,0 +1,57 @@
+package integration_test;
+
+import antonafanasjew.cosmodog.domains.WeaponType;
+import antonafanasjew.cosmodog.globals.CosmodogModelHolder;
+import antonafanasjew.cosmodog.model.actors.Player;
+import antonafanasjew.cosmodog.model.inventory.*;
+import antonafanasjew.cosmodog.model.upgrades.Weapon;
+import antonafanasjew.cosmodog.player.AbstractPlayerBuilder;
+import antonafanasjew.cosmodog.player.PlayerBuilder;
+import org.newdawn.slick.SlickException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestSnowDriftsOnRocks {
+
+	public static void main(String[] args) throws SlickException {
+		
+		PlayerBuilder playerBuilder = new AbstractPlayerBuilder() {
+			
+			@Override
+			protected void updatePlayer(Player player) {
+				player.setPositionX(7);
+				player.setPositionY(365);
+				player.setMaxLife(100);
+				player.setLife(100);
+				player.getInventory().put(InventoryItemType.AXE, new AxeInventoryItem());
+				player.getInventory().put(InventoryItemType.PICK, new PickInventoryItem());
+				player.getInventory().put(InventoryItemType.SKI, new SkiInventoryItem());
+				player.getInventory().put(InventoryItemType.MACHETE, new MacheteInventoryItem());
+				player.getInventory().put(InventoryItemType.JACKET, new JacketInventoryItem());
+
+				player.getArsenal().addWeaponToArsenal(new Weapon(WeaponType.RPG));
+				player.getArsenal().addWeaponToArsenal(new Weapon(WeaponType.SHOTGUN));
+
+
+				List<String> debuggerPositions = new ArrayList<>();
+				debuggerPositions.add("16/392");
+				debuggerPositions.add("87/388");
+				debuggerPositions.add("114/385");
+				debuggerPositions.add("157/384");
+				debuggerPositions.add("224/392");
+				debuggerPositions.add("267/392");
+				debuggerPositions.add("393/324");
+				debuggerPositions.add("360/320");
+				debuggerPositions.add("7/365");
+				player.getInventory().put(InventoryItemType.DEBUGGER, new DebuggerInventoryItem(String.join(";", debuggerPositions)));
+
+			}
+		};
+
+		CosmodogModelHolder.replacePlayerBuilder(playerBuilder);
+
+		TestStarter.main(args);
+	}
+	
+}
