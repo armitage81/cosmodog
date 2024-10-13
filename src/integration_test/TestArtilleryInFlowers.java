f4b518fca82df8a9674e80be09b893a9fb3d8001
Anton Afanasjew <contact@anton-afanasjew.de>
2024-10-13 08:06:11 +0200

    #127 - Artilleries in the ruins in the center of the map are easier avoidable now.

---
+++ src/integration_test/TestArtilleryInFlowers.java
@@ -0,0 +1,33 @@
+package integration_test;
+
+import antonafanasjew.cosmodog.globals.CosmodogModelHolder;
+import antonafanasjew.cosmodog.model.actors.Player;
+import antonafanasjew.cosmodog.model.actors.Vehicle;
+import antonafanasjew.cosmodog.model.inventory.InventoryItemType;
+import antonafanasjew.cosmodog.model.inventory.VehicleInventoryItem;
+import antonafanasjew.cosmodog.player.AbstractPlayerBuilder;
+import antonafanasjew.cosmodog.player.PlayerBuilder;
+import org.newdawn.slick.SlickException;
+
+public class TestArtilleryInFlowers {
+
+	public static void main(String[] args) throws SlickException {
+		
+		PlayerBuilder playerBuilder = new AbstractPlayerBuilder() {
+			
+			@Override
+			protected void updatePlayer(Player player) {
+				player.setPositionX(57);
+				player.setPositionY(150);
+				player.setMaxLife(100);
+				player.setLife(100);
+				
+			}
+		};
+
+		CosmodogModelHolder.replacePlayerBuilder(playerBuilder);
+
+		TestStarter.main(args);
+	}
+	
+}
