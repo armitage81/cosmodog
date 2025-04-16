c4dc5706f8524346db5917bc40f803606ff2fa88
Armitage81 <contact@anton-afanasjew.de>
2025-04-16 22:15:50 +0200

    Implemented portal chambers 12-14.

--- src/integration_test/TestPortals.java
+++ src/integration_test/TestPortals.java
@@ -18,7 +18,7 @@ public class TestPortals {
 			
 			@Override
 			protected void updatePlayer(Player player) {
-				player.setPosition(Position.fromCoordinates(198, 75, MapType.SPACE));
+				player.setPosition(Position.fromCoordinates(165, 76, MapType.SPACE));
 				player.setMaxLife(100);
 				player.setLife(100);
 				player.getInventory().put(InventoryItemType.BINOCULARS, new BinocularsInventoryItem());
