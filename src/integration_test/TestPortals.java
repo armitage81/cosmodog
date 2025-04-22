01738c1b0e5366a854a50ae7bbd36bbe3abcf1f5
Anton Afanasjew <contact@anton-afanasjew.de>
2025-04-22 07:58:40 +0200

    Implemented portal chambers 16-17

--- src/integration_test/TestPortals.java
+++ src/integration_test/TestPortals.java
@@ -18,7 +18,7 @@ public class TestPortals {
 			
 			@Override
 			protected void updatePlayer(Player player) {
-				player.setPosition(Position.fromCoordinates(150, 78, MapType.SPACE));
+				player.setPosition(Position.fromCoordinates(125, 93, MapType.SPACE));
 				player.setMaxLife(100);
 				player.setLife(100);
 				player.getInventory().put(InventoryItemType.BINOCULARS, new BinocularsInventoryItem());
