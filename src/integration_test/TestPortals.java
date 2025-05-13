faeb2b571e39b9336325dbcaaaba64f58432592e
Armitage81 <contact@anton-afanasjew.de>
2025-05-13 13:52:44 +0200

    Space lab deco design

--- src/integration_test/TestPortals.java
+++ src/integration_test/TestPortals.java
@@ -33,7 +33,7 @@ public class TestPortals {
 			
 			@Override
 			protected void updatePlayer(Player player) {
-				player.setPosition(Position.fromCoordinates(177, 60, MapType.SPACE));
+				player.setPosition(Position.fromCoordinates(207, 60, MapType.SPACE));
 				player.setMaxLife(100);
 				player.setLife(100);
 				player.getInventory().put(InventoryItemType.BINOCULARS, new BinocularsInventoryItem());
