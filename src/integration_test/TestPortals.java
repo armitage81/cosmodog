7b1e558f9b395b0c9ebd6c7690009186813375f3
Armitage81 <contact@anton-afanasjew.de>
2025-06-19 22:54:39 +0200

    #169 - Completing the portal puzzle series is rewarded with a monolith.

--- src/integration_test/TestPortals.java
+++ src/integration_test/TestPortals.java
@@ -34,7 +34,7 @@ public class TestPortals {
 			
 			@Override
 			protected void updatePlayer(Player player) {
-				player.setPosition(Position.fromCoordinates(221, 131, MapType.SPACE));
+				player.setPosition(Position.fromCoordinates(174, 167, MapType.SPACE));
 				player.setMaxLife(100);
 				player.setLife(100);
 				player.getInventory().put(InventoryItemType.PORTAL_GUN, new PortalGunInventoryItem());
