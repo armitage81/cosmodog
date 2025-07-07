6007912dfb733a0362783914efb2e597659676d3
Anton Afanasjew <contact@anton-afanasjew.de>
2025-07-07 07:29:35 +0200

    #212 - Hidden passage in the poisonous labyrinth is more obvious now.

---
+++ src/integration_test/TestNotObviousPassageInPoisonLabyrinth.java
@@ -0,0 +1,36 @@
+package integration_test;
+
+import antonafanasjew.cosmodog.domains.MapType;
+import antonafanasjew.cosmodog.globals.CosmodogModelHolder;
+import antonafanasjew.cosmodog.model.actors.Player;
+import antonafanasjew.cosmodog.model.actors.Vehicle;
+import antonafanasjew.cosmodog.model.inventory.InventoryItemType;
+import antonafanasjew.cosmodog.model.inventory.VehicleInventoryItem;
+import antonafanasjew.cosmodog.player.AbstractPlayerBuilder;
+import antonafanasjew.cosmodog.player.PlayerBuilder;
+import antonafanasjew.cosmodog.topology.Position;
+import org.newdawn.slick.SlickException;
+
+public class TestNotObviousPassageInPoisonLabyrinth {
+
+	public static void main(String[] args) throws SlickException {
+		
+		PlayerBuilder playerBuilder = new AbstractPlayerBuilder() {
+			
+			@Override
+			protected void updatePlayer(Player player) {
+				player.setPosition(Position.fromCoordinates(239, 90, MapType.MAIN));
+				player.setMaxLife(100);
+				player.setLife(100);
+				VehicleInventoryItem vii = new VehicleInventoryItem(new Vehicle());
+				player.getInventory().put(InventoryItemType.VEHICLE, vii);
+				
+			}
+		};
+
+		CosmodogModelHolder.replacePlayerBuilder(playerBuilder);
+
+		TestStarter.main(args);
+	}
+	
+}
