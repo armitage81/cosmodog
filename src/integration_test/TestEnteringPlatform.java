194200fce5eec7333ece783cf727f11c7855d0f9
Armitage81 <contact@anton-afanasjew.de>
2025-07-11 22:35:17 +0200

    #228 - When entering platform, a cutscene plays now.

---
+++ src/integration_test/TestEnteringPlatform.java
@@ -0,0 +1,42 @@
+package integration_test;
+
+import antonafanasjew.cosmodog.domains.MapType;
+import antonafanasjew.cosmodog.globals.CosmodogModelHolder;
+import antonafanasjew.cosmodog.model.actors.Platform;
+import antonafanasjew.cosmodog.model.actors.Player;
+import antonafanasjew.cosmodog.model.inventory.*;
+import antonafanasjew.cosmodog.player.AbstractPlayerBuilder;
+import antonafanasjew.cosmodog.player.PlayerBuilder;
+import antonafanasjew.cosmodog.topology.Position;
+import org.newdawn.slick.SlickException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestEnteringPlatform {
+
+	public static void main(String[] args) throws SlickException {
+		
+		PlayerBuilder playerBuilder = new AbstractPlayerBuilder() {
+			
+			@Override
+			protected void updatePlayer(Player player) {
+				player.setPosition(Position.fromCoordinates(355, 389, MapType.MAIN));
+				player.setMaxLife(100);
+				player.setLife(100);
+				player.getGameProgress().setWormActive(false);
+				player.getInventory().put(InventoryItemType.SKI, new SkiInventoryItem());
+				player.getInventory().put(InventoryItemType.JACKET, new JacketInventoryItem());
+
+				List<String> debuggerPositions = new ArrayList<>();
+				debuggerPositions.add(String.format("%s/%s/%s", 355, 389, MapType.MAIN));
+				player.getInventory().put(InventoryItemType.DEBUGGER, new DebuggerInventoryItem(String.join(";", debuggerPositions)));
+			}
+		};
+
+		CosmodogModelHolder.replacePlayerBuilder(playerBuilder);
+
+		TestStarter.main(args);
+	}
+	
+}
