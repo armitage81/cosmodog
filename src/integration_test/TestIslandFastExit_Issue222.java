f62113a7f5fc8ee62added8b6783aa9aa8bc93e7
Armitage81 <contact@anton-afanasjew.de>
2025-07-11 21:27:56 +0200

    #222 - A fast exit from an island is addded now.

---
+++ src/integration_test/TestIslandFastExit_Issue222.java
@@ -0,0 +1,36 @@
+package integration_test;
+
+import antonafanasjew.cosmodog.domains.MapType;
+import antonafanasjew.cosmodog.globals.CosmodogModelHolder;
+import antonafanasjew.cosmodog.model.actors.Player;
+import antonafanasjew.cosmodog.model.inventory.BoatInventoryItem;
+import antonafanasjew.cosmodog.model.inventory.DebuggerInventoryItem;
+import antonafanasjew.cosmodog.model.inventory.InventoryItemType;
+import antonafanasjew.cosmodog.player.AbstractPlayerBuilder;
+import antonafanasjew.cosmodog.player.PlayerBuilder;
+import antonafanasjew.cosmodog.topology.Position;
+import org.newdawn.slick.SlickException;
+
+public class TestIslandFastExit_Issue222 {
+
+    public static void main(String[] args) throws SlickException {
+
+        PlayerBuilder playerBuilder = new AbstractPlayerBuilder() {
+
+            @Override
+            protected void updatePlayer(Player player) {
+                player.setPosition(Position.fromCoordinates(261, 41, MapType.MAIN));
+                player.setMaxLife(100);
+                player.setLife(100);
+                player.getInventory().put(InventoryItemType.BOAT, new BoatInventoryItem());
+                player.getInventory().put(InventoryItemType.DEBUGGER, new DebuggerInventoryItem("261/37;261/41"));
+
+
+            }
+        };
+
+        CosmodogModelHolder.replacePlayerBuilder(playerBuilder);
+
+        TestStarter.main(args);
+    }
+}
