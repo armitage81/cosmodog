49c297257db5812fdfe0f53a2924da4a5b2cb4e5
Anton Afanasjew <contact@anton-afanasjew.de>
2025-07-12 18:08:46 +0200

    #231 - Pick axe has better contrast now when found.

--- src/integration_test/TestPickAxeRetrieval.java
+++ src/integration_test/TestPickAxeRetrieval.java
@@ -1,12 +1,14 @@
 package integration_test;
 
 import antonafanasjew.cosmodog.domains.MapType;
+import antonafanasjew.cosmodog.domains.WeaponType;
 import antonafanasjew.cosmodog.globals.CosmodogModelHolder;
 import antonafanasjew.cosmodog.model.actors.Player;
 import antonafanasjew.cosmodog.model.inventory.DebuggerInventoryItem;
 import antonafanasjew.cosmodog.model.inventory.InventoryItemType;
 import antonafanasjew.cosmodog.model.inventory.JacketInventoryItem;
 import antonafanasjew.cosmodog.model.inventory.SkiInventoryItem;
+import antonafanasjew.cosmodog.model.upgrades.Weapon;
 import antonafanasjew.cosmodog.player.AbstractPlayerBuilder;
 import antonafanasjew.cosmodog.player.PlayerBuilder;
 import antonafanasjew.cosmodog.topology.Position;
@@ -21,9 +23,9 @@ public class TestPickAxeRetrieval {
 			@Override
 			protected void updatePlayer(Player player) {
 				player.setPosition(Position.fromCoordinates(291, 100, MapType.MAIN));
-				player.setLife(1);
-				player.getInventory().put(InventoryItemType.SKI, new SkiInventoryItem());
-				player.getInventory().put(InventoryItemType.JACKET, new JacketInventoryItem());
+				player.setMaxLife(100);
+				player.setLife(100);
+				player.getArsenal().addWeaponToArsenal(new Weapon(WeaponType.RPG));
 				player.getInventory().put(InventoryItemType.DEBUGGER, new DebuggerInventoryItem("291/100"));
 			}
 		};
