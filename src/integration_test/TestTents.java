6fc5cef32cc874b935f600c0345fa7600c66b8d0
Anton Afanasjew <contact@anton-afanasjew.de>
2025-07-12 21:32:06 +0200

    #234 - Items in tents are desaturated now.

---
+++ src/integration_test/TestTents.java
@@ -0,0 +1,44 @@
+package integration_test;
+
+import antonafanasjew.cosmodog.domains.MapType;
+import antonafanasjew.cosmodog.domains.WeaponType;
+import antonafanasjew.cosmodog.globals.CosmodogModelHolder;
+import antonafanasjew.cosmodog.model.actors.Player;
+import antonafanasjew.cosmodog.model.inventory.DebuggerInventoryItem;
+import antonafanasjew.cosmodog.model.inventory.InventoryItemType;
+import antonafanasjew.cosmodog.model.inventory.JacketInventoryItem;
+import antonafanasjew.cosmodog.model.inventory.SkiInventoryItem;
+import antonafanasjew.cosmodog.model.upgrades.Weapon;
+import antonafanasjew.cosmodog.player.AbstractPlayerBuilder;
+import antonafanasjew.cosmodog.player.PlayerBuilder;
+import antonafanasjew.cosmodog.topology.Position;
+import org.newdawn.slick.SlickException;
+
+public class TestTents {
+
+	/**
+	 * - The items in tents must be desaturated to not be confused with interactive elements of the game.
+	 */
+
+	public static void main(String[] args) throws SlickException {
+		
+		PlayerBuilder playerBuilder = new AbstractPlayerBuilder() {
+			
+			@Override
+			protected void updatePlayer(Player player) {
+				player.setPosition(Position.fromCoordinates(341, 186, MapType.MAIN));
+				player.setMaxLife(100);
+				player.setLife(100);
+				player.getArsenal().addWeaponToArsenal(new Weapon(WeaponType.SHOTGUN));
+				player.getArsenal().addWeaponToArsenal(new Weapon(WeaponType.SHOTGUN));
+				player.getArsenal().addWeaponToArsenal(new Weapon(WeaponType.SHOTGUN));
+				player.getInventory().put(InventoryItemType.DEBUGGER, new DebuggerInventoryItem("324/174;316/183;329/209;331/219;322/235;335/248;341/186"));
+			}
+		};
+
+		CosmodogModelHolder.replacePlayerBuilder(playerBuilder);
+
+		TestStarter.main(args);
+	}
+	
+}
