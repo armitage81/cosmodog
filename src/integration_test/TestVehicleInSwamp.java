f1c13337fddb3efe87a832502b43126a20056c83
Anton Afanasjew <contact@anton-afanasjew.de>
2025-07-13 21:53:01 +0200

    #242 - Vehicles are blocked before the first bridge in the swamp now.

---
+++ src/integration_test/TestVehicleInSwamp.java
@@ -0,0 +1,45 @@
+package integration_test;
+
+import antonafanasjew.cosmodog.domains.MapType;
+import antonafanasjew.cosmodog.domains.WeaponType;
+import antonafanasjew.cosmodog.globals.CosmodogModelHolder;
+import antonafanasjew.cosmodog.model.actors.Platform;
+import antonafanasjew.cosmodog.model.actors.Player;
+import antonafanasjew.cosmodog.model.actors.Vehicle;
+import antonafanasjew.cosmodog.model.inventory.DebuggerInventoryItem;
+import antonafanasjew.cosmodog.model.inventory.InventoryItemType;
+import antonafanasjew.cosmodog.model.inventory.PlatformInventoryItem;
+import antonafanasjew.cosmodog.model.inventory.VehicleInventoryItem;
+import antonafanasjew.cosmodog.model.upgrades.Weapon;
+import antonafanasjew.cosmodog.player.AbstractPlayerBuilder;
+import antonafanasjew.cosmodog.player.PlayerBuilder;
+import antonafanasjew.cosmodog.topology.Position;
+import org.newdawn.slick.SlickException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestVehicleInSwamp {
+
+	public static void main(String[] args) throws SlickException {
+		
+		PlayerBuilder playerBuilder = new AbstractPlayerBuilder() {
+			
+			@Override
+			protected void updatePlayer(Player player) {
+				player.setPosition(Position.fromCoordinates(299, 137, MapType.MAIN));
+				player.setMaxLife(100);
+				player.setLife(100);
+				player.getArsenal().addWeaponToArsenal(new Weapon(WeaponType.SHOTGUN));
+				player.getGameProgress().setArmors(9);
+				player.getGameProgress().setFuelTanks(6);
+				player.getInventory().put(InventoryItemType.VEHICLE, new VehicleInventoryItem(new Vehicle()));
+			}
+		};
+
+		CosmodogModelHolder.replacePlayerBuilder(playerBuilder);
+
+		TestStarter.main(args);
+	}
+	
+}
