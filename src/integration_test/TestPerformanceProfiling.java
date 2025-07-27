f9b1bc0ad79704d58d4f1b807c672cc4d2f76f12
Anton Afanasjew <contact@anton-afanasjew.de>
2025-07-27 11:52:04 +0200

    #270 - Map scrolling is faster now.

--- src/integration_test/TestPerformanceProfiling.java
+++ src/integration_test/TestPerformanceProfiling.java
@@ -3,10 +3,7 @@ package integration_test;
 import antonafanasjew.cosmodog.domains.MapType;
 import antonafanasjew.cosmodog.globals.CosmodogModelHolder;
 import antonafanasjew.cosmodog.model.actors.Player;
-import antonafanasjew.cosmodog.model.inventory.AntidoteInventoryItem;
-import antonafanasjew.cosmodog.model.inventory.BinocularsInventoryItem;
-import antonafanasjew.cosmodog.model.inventory.DebuggerInventoryItem;
-import antonafanasjew.cosmodog.model.inventory.InventoryItemType;
+import antonafanasjew.cosmodog.model.inventory.*;
 import antonafanasjew.cosmodog.player.AbstractPlayerBuilder;
 import antonafanasjew.cosmodog.player.PlayerBuilder;
 import antonafanasjew.cosmodog.topology.Position;
@@ -26,6 +23,17 @@ public class TestPerformanceProfiling {
 				player.getInventory().put(InventoryItemType.ANTIDOTE, new AntidoteInventoryItem());
 				player.getInventory().put(InventoryItemType.BINOCULARS, new BinocularsInventoryItem());
 				player.getInventory().put(InventoryItemType.DEBUGGER, new DebuggerInventoryItem());
+
+				ChartInventoryItem chart = new ChartInventoryItem();
+
+				for (int i = 0; i < 8; i++) {
+					for (int j = 0; j < 8; j++) {
+						chart.discoverPiece(i,j);
+					}
+				}
+
+
+				player.getInventory().put(InventoryItemType.CHART, chart);
 
 			}
 		};
