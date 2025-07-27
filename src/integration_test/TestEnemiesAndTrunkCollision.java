1eea5e24981e7c584246d4745362e8b123301778
Anton Afanasjew <contact@anton-afanasjew.de>
2025-07-27 12:37:13 +0200

    There is a test case now, handling enemies' trunk collision.

---
+++ src/integration_test/TestEnemiesAndTrunkCollision.java
@@ -0,0 +1,33 @@
+package integration_test;
+
+import antonafanasjew.cosmodog.domains.MapType;
+import antonafanasjew.cosmodog.globals.CosmodogModelHolder;
+import antonafanasjew.cosmodog.model.actors.Player;
+import antonafanasjew.cosmodog.model.inventory.BinocularsInventoryItem;
+import antonafanasjew.cosmodog.model.inventory.InventoryItemType;
+import antonafanasjew.cosmodog.player.AbstractPlayerBuilder;
+import antonafanasjew.cosmodog.player.PlayerBuilder;
+import antonafanasjew.cosmodog.topology.Position;
+import org.newdawn.slick.SlickException;
+
+public class TestEnemiesAndTrunkCollision {
+
+	public static void main(String[] args) throws SlickException {
+		
+		PlayerBuilder playerBuilder = new AbstractPlayerBuilder() {
+			
+			@Override
+			protected void updatePlayer(Player player) {
+				player.setPosition(Position.fromCoordinates(49, 38, MapType.ALTERNATIVE));
+				player.setMaxLife(100);
+				player.setLife(100);
+				player.getInventory().put(InventoryItemType.BINOCULARS, new BinocularsInventoryItem());
+			}
+		};
+
+		CosmodogModelHolder.replacePlayerBuilder(playerBuilder);
+
+		TestStarter.main(args);
+	}
+	
+}
