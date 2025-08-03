32e50d2b9c034c8aee5c117cd81d34d474fdfd62
Armitage81 <contact@anton-afanasjew.de>
2025-08-03 21:00:51 +0200

    #353 - The crater has a new sprite now.

---
+++ src/integration_test/TestCraterSprite.java
@@ -0,0 +1,36 @@
+package integration_test;
+
+import antonafanasjew.cosmodog.domains.MapType;
+import antonafanasjew.cosmodog.globals.CosmodogModelHolder;
+import antonafanasjew.cosmodog.model.actors.Player;
+import antonafanasjew.cosmodog.model.actors.Vehicle;
+import antonafanasjew.cosmodog.model.inventory.InventoryItemType;
+import antonafanasjew.cosmodog.model.inventory.RadioactiveSuitInventoryItem;
+import antonafanasjew.cosmodog.model.inventory.VehicleInventoryItem;
+import antonafanasjew.cosmodog.player.AbstractPlayerBuilder;
+import antonafanasjew.cosmodog.player.PlayerBuilder;
+import antonafanasjew.cosmodog.topology.Position;
+import org.newdawn.slick.SlickException;
+
+public class TestCraterSprite {
+
+	public static void main(String[] args) throws SlickException {
+		
+		PlayerBuilder playerBuilder = new AbstractPlayerBuilder() {
+			
+			@Override
+			protected void updatePlayer(Player player) {
+				player.setPosition(Position.fromCoordinates(115, 156, MapType.MAIN));
+				player.setMaxLife(100);
+				player.setLife(100);
+				player.getInventory().put(InventoryItemType.RADIOACTIVESUIT, new RadioactiveSuitInventoryItem());
+				
+			}
+		};
+
+		CosmodogModelHolder.replacePlayerBuilder(playerBuilder);
+
+		TestStarter.main(args);
+	}
+	
+}
