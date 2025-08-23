9eb7a1c8a630b6a910de5e5728aa2f638ca7ae54
Anton Afanasjew <contact@anton-afanasjew.de>
2025-08-23 15:55:19 +0200

    #273 - Plane sprites look better now.

---
+++ src/integration_test/TestPlanes.java
@@ -0,0 +1,44 @@
+package integration_test;
+
+import antonafanasjew.cosmodog.domains.MapType;
+import antonafanasjew.cosmodog.domains.WeaponType;
+import antonafanasjew.cosmodog.globals.CosmodogModelHolder;
+import antonafanasjew.cosmodog.model.actors.Player;
+import antonafanasjew.cosmodog.model.actors.Vehicle;
+import antonafanasjew.cosmodog.model.inventory.DebuggerInventoryItem;
+import antonafanasjew.cosmodog.model.inventory.InventoryItemType;
+import antonafanasjew.cosmodog.model.inventory.VehicleInventoryItem;
+import antonafanasjew.cosmodog.model.upgrades.Weapon;
+import antonafanasjew.cosmodog.player.AbstractPlayerBuilder;
+import antonafanasjew.cosmodog.player.PlayerBuilder;
+import antonafanasjew.cosmodog.topology.Position;
+import org.newdawn.slick.SlickException;
+
+public class TestPlanes {
+
+	public static void main(String[] args) throws SlickException {
+		
+		PlayerBuilder playerBuilder = new AbstractPlayerBuilder() {
+			
+			@Override
+			protected void updatePlayer(Player player) {
+				player.setPosition(Position.fromCoordinates(130, 137, MapType.MAIN));
+				player.setMaxLife(100);
+				player.setLife(100);
+				player.getArsenal().addWeaponToArsenal(new Weapon(WeaponType.RPG));
+				player.getArsenal().addWeaponToArsenal(new Weapon(WeaponType.SHOTGUN));
+				player.getArsenal().addWeaponToArsenal(new Weapon(WeaponType.RIFLE));
+				player.getArsenal().addWeaponToArsenal(new Weapon(WeaponType.MACHINEGUN));
+				player.getArsenal().addWeaponToArsenal(new Weapon(WeaponType.PISTOL));
+
+				DebuggerInventoryItem debugger = new DebuggerInventoryItem("346/264;130/137");
+				player.getInventory().put(InventoryItemType.DEBUGGER, debugger);
+			}
+		};
+
+		CosmodogModelHolder.replacePlayerBuilder(playerBuilder);
+
+		TestStarter.main(args);
+	}
+	
+}
