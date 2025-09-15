e1964fed786c1b326d8e90aad3de85a6d6bd3e9f
Anton Afanasjew <contact@anton-afanasjew.de>
2025-09-15 21:05:39 +0200

    #313 - Alisa warns Karl about the snow worm now.

---
+++ src/integration_test/TestSnowworm.java
@@ -0,0 +1,34 @@
+package integration_test;
+
+import antonafanasjew.cosmodog.domains.MapType;
+import antonafanasjew.cosmodog.globals.CosmodogModelHolder;
+import antonafanasjew.cosmodog.model.actors.Player;
+import antonafanasjew.cosmodog.model.inventory.*;
+import antonafanasjew.cosmodog.player.AbstractPlayerBuilder;
+import antonafanasjew.cosmodog.player.PlayerBuilder;
+import antonafanasjew.cosmodog.topology.Position;
+import org.newdawn.slick.SlickException;
+
+public class TestSnowworm {
+
+	public static void main(String[] args) throws SlickException {
+		
+		PlayerBuilder playerBuilder = new AbstractPlayerBuilder() {
+			
+			@Override
+			protected void updatePlayer(Player player) {
+				player.setPosition(Position.fromCoordinates(297, 380, MapType.MAIN));
+				player.setLife(100);
+				player.getInventory().put(InventoryItemType.SKI, new SkiInventoryItem());
+				player.getInventory().put(InventoryItemType.JACKET, new JacketInventoryItem());
+				player.getInventory().put(InventoryItemType.BINOCULARS, new BinocularsInventoryItem());
+				player.getInventory().put(InventoryItemType.DEBUGGER, new DebuggerInventoryItem("297/380"));
+			}
+		};
+
+		CosmodogModelHolder.replacePlayerBuilder(playerBuilder);
+
+		TestStarter.main(args);
+	}
+	
+}
