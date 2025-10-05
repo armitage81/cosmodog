cde4a0d1a129f5679330b1b9eb2ec6a35763aac6
Anton Afanasjew <contact@anton-afanasjew.de>
2025-10-05 20:00:08 +0200

    #404, #405 - Secrets reached by teleporters are being registered properly now.

---
+++ src/integration_test/TestTeleporterSecrets.java
@@ -0,0 +1,36 @@
+package integration_test;
+
+import antonafanasjew.cosmodog.domains.MapType;
+import antonafanasjew.cosmodog.globals.CosmodogModelHolder;
+import antonafanasjew.cosmodog.model.actors.Player;
+import antonafanasjew.cosmodog.model.actors.Vehicle;
+import antonafanasjew.cosmodog.model.inventory.DebuggerInventoryItem;
+import antonafanasjew.cosmodog.model.inventory.InventoryItemType;
+import antonafanasjew.cosmodog.model.inventory.VehicleInventoryItem;
+import antonafanasjew.cosmodog.player.AbstractPlayerBuilder;
+import antonafanasjew.cosmodog.player.PlayerBuilder;
+import antonafanasjew.cosmodog.topology.Position;
+import org.newdawn.slick.SlickException;
+
+public class TestTeleporterSecrets {
+
+	public static void main(String[] args) throws SlickException {
+		
+		PlayerBuilder playerBuilder = new AbstractPlayerBuilder() {
+			
+			@Override
+			protected void updatePlayer(Player player) {
+				player.setPosition(Position.fromCoordinates(63, 86, MapType.MAIN));
+				player.setMaxLife(100);
+				player.setLife(100);
+				player.getInventory().put(InventoryItemType.DEBUGGER, new DebuggerInventoryItem("158/97;63/86"));
+				
+			}
+		};
+
+		CosmodogModelHolder.replacePlayerBuilder(playerBuilder);
+
+		TestStarter.main(args);
+	}
+	
+}
